44f99898d985e9f10b59bd78b2e42a8a7eb3c0c1
Gia Bảo <hgb131012@gmail.com>
2026-07-04 11:43:42 +0700

    Create Birthday_Cake_Candles_.py

---
+++ Birthday_Cake_Candles_.py
@@ -0,0 +1,13 @@
+from collections import Counter
+import os
+
+def birthdayCakeCandles(candles):
+    return Counter(candles).most_common()[0][1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    candles_count = int(input().strip())
+    candles = list(map(int, input().rstrip().split()))
+    result = birthdayCakeCandles(candles)
+    fptr.write(str(result) + '\n')
+    fptr.close()
